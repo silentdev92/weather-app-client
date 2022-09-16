e0f0f7cb6669fec841ab10fe246e998f17dd4f4d
Ivan Zuev <113107003+silentdev92@users.noreply.github.com>
2022-09-16 17:35:23 +0800

    feat: Created ForecastShort ui

--- src/components/ForecastShort/ForecastShort.tsx
+++ src/components/ForecastShort/ForecastShort.tsx
@@ -1,7 +1,64 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
+import classNames from 'classnames/bind'
+import styles from './ForecastShort.module.sass'
+import { ForecastSmallCard } from '../ForecastSmallCard'
+
+let cx = classNames.bind(styles)
+
+type ForecastDay = 'today' | 'tomorrow'
 
 const ForecastShort: FC = () => {
-  return <div>ForecastShort</div>
+  const [forecastDay, setForecastDay] = useState<ForecastDay>('today')
+
+  const changeForecastDay = (day: ForecastDay) => {
+    if (day !== forecastDay) setForecastDay(day)
+  }
+
+  return (
+    <div className={styles.root}>
+      <div className={styles.nav}>
+        <div className={styles.left}>
+          <span
+            className={cx('item', { active: forecastDay === 'today' })}
+            onClick={() => changeForecastDay('today')}
+          >
+            Today
+          </span>
+          <span
+            className={cx('item', { active: forecastDay === 'tomorrow' })}
+            onClick={() => changeForecastDay('tomorrow')}
+          >
+            Tomorrow
+          </span>
+        </div>
+        <div className={styles.right}>
+          <span className={styles.item}>Next 5 Days</span>
+          <div className={styles.icon}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M8.25 4.5l7.5 7.5-7.5 7.5"
+              />
+            </svg>
+          </div>
+        </div>
+      </div>
+      <div className={styles.main}>
+        {[...Array(20)].map((_, idx) => (
+          <div className={styles.item} key={idx}>
+            <ForecastSmallCard />
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default ForecastShort

9f73c8b9b259898dd4216de645371220c09ca8f1
Ivan Zuev <113107003+silentdev92@users.noreply.github.com>
2022-09-19 17:12:08 +0800

    feat: Created Button ui component

---
+++ src/components/ui/Button/Button.tsx
@@ -0,0 +1,29 @@
+import React, { FC } from 'react'
+import styles from './Button.module.sass'
+
+interface ButtonProps {
+  text: string
+  type?: 'button' | 'submit'
+  disabled?: boolean
+  onClick: () => void
+}
+
+const Button: FC<ButtonProps> = ({
+  text,
+  type = 'button',
+  disabled = false,
+  onClick,
+}) => {
+  return (
+    <button
+      className={styles.root}
+      onClick={onClick}
+      type={type}
+      disabled={disabled}
+    >
+      {text}
+    </button>
+  )
+}
+
+export default Button

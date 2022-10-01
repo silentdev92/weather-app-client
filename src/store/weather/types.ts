a51b8a89a28a5a734a5a6c1c5248bb2f9f24c3a1
Ivan Zuev <113107003+silentdev92@users.noreply.github.com>
2022-10-01 21:03:36 +0800

    feat: Created weather types

---
+++ src/store/weather/types.ts
@@ -0,0 +1,112 @@
+export interface CurrentWeatherData {
+  coord: {
+    lon: number
+    lat: number
+  }
+  weather: [
+    {
+      id: number
+      main: string
+      description: string
+      icon: string
+    }
+  ]
+  base: string
+  main: {
+    temp: number
+    feels_like: number
+    temp_min: number
+    temp_max: number
+    pressure: number
+    humidity: number
+  }
+  visibility: number
+  wind: {
+    speed: number
+    deg: number
+  }
+  clouds: {
+    all: number
+  }
+  dt: number
+  sys: {
+    type: number
+    id: number
+    country: string
+    sunrise: number
+    sunset: number
+  }
+  timezone: number
+  id: number
+  name: string
+  cod: number
+}
+
+export interface ForecastWeatherData {
+  cod: string
+  message: number
+  cnt: number
+  list: [
+    {
+      dt: number
+      main: {
+        temp: number
+        feels_like: number
+        temp_min: number
+        temp_max: number
+        pressure: number
+        sea_level: number
+        grnd_level: number
+        humidity: number
+        temp_kf: number
+      }
+      weather: [
+        {
+          id: number
+          main: string
+          description: string
+          icon: string
+        }
+      ]
+      clouds: {
+        all: number
+      }
+      wind: {
+        speed: number
+        deg: number
+        gust: number
+      }
+      visibility: number
+      pop: number
+      sys: {
+        pod: string
+      }
+      dt_txt: string
+    }
+  ]
+  city: {
+    id: number
+    name: string
+    coord: {
+      lat: number
+      lon: number
+    }
+    country: string
+    population: number
+    timezone: number
+    sunrise: number
+    sunset: number
+  }
+}
+
+export interface getCurrentWeatherQuery {
+  lat: number
+  lon: number
+  mode?: 'xml' | 'html'
+  units?: 'standard' | 'metric' | 'imperial'
+  lang?: string
+}
+
+export interface getForecastWeatherQuery extends getCurrentWeatherQuery {
+  cnt?: number
+}

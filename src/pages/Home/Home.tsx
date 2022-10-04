bea808bd621f404ef57595b5da66649c3af04b4d
Ivan Zuev <113107003+silentdev92@users.noreply.github.com>
2022-10-04 19:57:37 +0800

    feat: Added loading current weather data to Home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,26 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect } from 'react'
 import { CurrentPrimaryCard } from '../../components/CurrentPrimaryCard'
 import { CurrentSecondaryCard } from '../../components/CurrentSecondaryCard'
 import { ForecastShort } from '../../components/ForecastShort'
 import { OptionsCard } from '../../components/OptionsCard'
+import { useAppSelector } from '../../hooks/redux'
+import { selectCurrentLocation } from '../../store/location/selectors'
+import { useLazyGetCurrentWeatherQuery } from '../../store/weather/api'
+import { selectUnits } from '../../store/weather/selectors'
 import styles from './Home.module.sass'
 
 const Home: FC = () => {
+  const location = useAppSelector(selectCurrentLocation)
+  const units = useAppSelector(selectUnits)
+
+  const [fetchCurrentWeather, { data }] = useLazyGetCurrentWeatherQuery()
+
+  useEffect(() => {
+    if (location) {
+      fetchCurrentWeather({ lat: location.lat, lon: location.lon, units })
+    }
+  }, [location, units])
+
   return (
     <div className={styles.root}>
       <div className={styles.optionsCard}>
@@ -15,13 +30,21 @@ const Home: FC = () => {
         <CurrentPrimaryCard />
       </div>
       <div>
-        <CurrentSecondaryCard icon="umbrella" title="RainFall" value="3cm" />
+        <CurrentSecondaryCard icon="umbrella" title="RainFall" value="3 cm" />
       </div>
       <div>
-        <CurrentSecondaryCard icon="wind" title="Wind" value="19km/h" />
+        <CurrentSecondaryCard
+          icon="wind"
+          title="Wind"
+          value={data?.wind.speed + 'm/s'}
+        />
       </div>
       <div>
-        <CurrentSecondaryCard icon="humidity" title="Humidity" value="64%" />
+        <CurrentSecondaryCard
+          icon="humidity"
+          title="Humidity"
+          value={data?.main.humidity + '%'}
+        />
       </div>
       <div className={styles.forecastShort}>
         <ForecastShort />
